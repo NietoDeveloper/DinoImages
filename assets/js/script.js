423d3f638a09fce1cf9d81b085bf705dc1c53d56
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-02-04 17:24:11 -0500

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,5 @@ function toggleActive(e) {
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
